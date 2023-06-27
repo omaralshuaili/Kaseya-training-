06ef703be69f68897bea54f36d90850ff352c5f4
omaralshuaili <omar.alshuaili@datto.com>
2023-06-27 13:35:37 +0100

    changing the port for azure

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ const serverOptions = {
 
 https
   .createServer(serverOptions, app)
-  .listen(8080, () => console.log(`listening on 8080`));
+  .listen(1337, () => console.log(`listening on 1337`));
 
 
   app.get('/', (req, res) => {
