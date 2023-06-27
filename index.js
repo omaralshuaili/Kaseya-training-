d1f21212fe57ec54c9b6ff34b569ee0fa4881775
omaralshuaili <omar.alshuaili@datto.com>
2023-06-27 15:56:39 +0100

    change port attempt 4

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ const serverOptions = {
   key: fs.readFileSync("ssl/local.key"),
   cert: fs.readFileSync("ssl/local.cert"),
 };
-const port = process.env.port || 8000;
+const port =  8000;
 
 https
   .createServer(serverOptions, app)
